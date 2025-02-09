97a7c0351c63c90c248e1efb9d9cb9f23c6c8741
Teto <tetocoding@gmail.com>
2025-02-09 20:01:28 +0700

    if statement

--- main.gd
+++ main.gd
@@ -1,6 +1,30 @@
 extends Node
 
+var health = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("hello world")
+	$Label.text = "Hello World"
+	$Label.modulate = Color.GREEN
+	
+	print(health)
+	
+	
+func _input(event: InputEvent) -> void:
+	
+	# space ditekan
+	if event.is_action_pressed("my_action"):
+		if health <= 0:
+			print("lu mati")
+			health = 0
+		elif health != 0 and health <= 100 :
+			health -= 20
+			print("awww")
+		else:
+			print(health)
+		
+		$Label.modulate = Color.RED
+		
+	# space di release
+	if event.is_action_released('my_action'):
+		$Label.modulate = Color.GREEN
