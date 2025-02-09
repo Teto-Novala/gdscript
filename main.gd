b1294183b1e3c9fc1b7a18469c496373eb9b3914
Teto <tetocoding@gmail.com>
2025-02-09 20:06:25 +0700

    variabel

--- main.gd
+++ main.gd
@@ -1,30 +1,12 @@
 extends Node
 
-var health = 100
+# scope
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Label.text = "Hello World"
-	$Label.modulate = Color.GREEN
 	
-	print(health)
+	# var godot dan coolness cmn ada di func _ready
+	# tipe variabel bisa tanpa di declare
+	var godot = true
+	var coolness = 9001
 	
-	
-func _input(event: InputEvent) -> void:
-	
-	# space ditekan
-	if event.is_action_pressed("my_action"):
-		if health <= 0:
-			print("lu mati")
-			health = 0
-		elif health != 0 and health <= 100 :
-			health -= 20
-			print("awww")
-		else:
-			print(health)
-		
-		$Label.modulate = Color.RED
-		
-	# space di release
-	if event.is_action_released('my_action'):
-		$Label.modulate = Color.GREEN
