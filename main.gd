6045f79d8559f7918d295be496a10bc3f2c1c8cc
Teto <tetocoding@gmail.com>
2025-02-09 20:15:32 +0700

    varibel 1.1

--- main.gd
+++ main.gd
@@ -1,12 +1,34 @@
 extends Node
 
-# scope
+# casting
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	var number = 42
+	var text = "nomor : " + str(number)
+	print(text)
 	
-	# var godot dan coolness cmn ada di func _ready
-	# tipe variabel bisa tanpa di declare
-	var godot = true
-	var coolness = 9001
+	var pi = 3.14
+	print(int(pi))
+	
+	# position
+						#x	#y
+	var vec2 = Vector2(0.0,0.0)
+						#x	#y	#z
+	var vec3 = Vector3(0.0,0.0,0.0)
+	
+	vec3.x += 2
+	print(vec3)
+	
+	# gdscript termasuk ke dynamicly type
+	
+	# varibel bisa di declare type nya
+	var nomor:int = 4
+	
+	# variabel bisa dikasih nilai default sekaligus typenya
+	var def := "hello"
+	# nilai defaultnya hello typenya string
+	
+	#def = 89
+	# bakal error klo dganti
 	
