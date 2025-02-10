1de9e0f98830d4cb3443a860ff31515db6e90a7c
Teto <tetocoding@gmail.com>
2025-02-10 15:33:10 +0700

    get

--- main.gd
+++ main.gd
@@ -1,17 +1,19 @@
 extends Node
 
-# set
+# get
 
-signal health_changed(new_health)
-
-var health := 100:
+var change := 0.2
+var change_pct: int:
+	get:
+		return change * 100
 	set(value):
-		health = clamp(value,0,100)
-		health_changed.emit(health)
+		change = float(value) / 100.0
 
 func _ready():
-	health = -345
-
-
-func _on_health_changed(new_health: Variant) -> void:
-	print(new_health)
+	print(change_pct)
+	change = 0.7
+	print(change_pct)
+	
+	change_pct = 30
+	print(change_pct)
+	
